64a0d72bd2bfa0d85e384008483ec413c4c79ed7
mihett05 <mihett05@yandex.ru>
2021-04-19 19:50:06 +0500

    Added task page

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
-import { Layout, PageHeader, Button, Col, Row, Menu } from 'antd';
+import { Link, useLocation } from 'react-router-dom';
 
-export default function AppLayout() {
+import {
+  Container,
+  Flex,
+  Spacer,
+  Heading,
+  Image,
+  Icon,
+  Center,
+  WrapItem,
+  Wrap,
+} from '@chakra-ui/react';
+import { BsGear, BsGearFill } from 'react-icons/all';
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  const location = useLocation();
+
   return (
     <div>
-      <PageHeader
-        title="Name"
-        extra={[<Button key="3">3</Button>, <Button key="2">2</Button>]}
-      />
-      <Row gutter={2}>
-        <Col>
-          <Menu mode="inline">
-            <Menu.SubMenu key="s1" title="Курсы">
-              <Menu.ItemGroup key="g1" title="Курсы">
-                <Menu.Item key="1">Option 1</Menu.Item>
-              </Menu.ItemGroup>
-            </Menu.SubMenu>
-          </Menu>
-        </Col>
-        <Col>2</Col>
-      </Row>
+      <Flex
+        bg="gray.100"
+        style={{
+          padding: '0.5vh 3vw 3vh 3vw',
+        }}
+      >
+        <Wrap>
+          <WrapItem>
+            <Center h="48px">
+              <Link to="/">
+                <Heading>Logo</Heading>
+              </Link>
+            </Center>
+          </WrapItem>
+        </Wrap>
+
+        <Spacer />
+        <Image
+          borderRadius="full"
+          boxSize="48px"
+          src="https://avatars.githubusercontent.com/u/26022093?v=4"
+        />
+        <Wrap
+          style={{
+            paddingLeft: '0.5vw',
+          }}
+        >
+          <WrapItem>
+            <Center w="48px" h="48px">
+              <Link to="/settings">
+                <Icon
+                  as={location.pathname === '/settings' ? BsGearFill : BsGear}
+                  w="10"
+                  h="10"
+                />
+              </Link>
+            </Center>
+          </WrapItem>
+        </Wrap>
+      </Flex>
+      <Container
+        maxW="container.xl"
+        style={{
+          paddingTop: '5vh',
+        }}
+      >
+        {children}
+      </Container>
     </div>
   );
 }
