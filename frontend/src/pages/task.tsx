45222a844f18f1d19da0fb506bab6e5c6ebb253b
mihett05 <mihett05@yandex.ru>
2021-04-20 21:32:19 +0500

    Added chat

--- frontend/src/pages/task.tsx
+++ frontend/src/pages/task.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Heading } from '@chakra-ui/react';
 
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import TaskInfo from '../components/TaskInfo';
+import Chat from '../components/Chat';
 
 export default function TaskPage() {
   return (
@@ -31,8 +32,25 @@ export default function TaskPage() {
         </Flex>
       </Box>
       <Box w="3%" />
-      <Box w="22%" h="75vh" bg="gray.100" borderRadius="md">
-        123
+      <Box w="25%" h="75vh" bg="gray.50" borderRadius="md">
+        <Flex direction="column" h="100%">
+          <Box
+            w="100%"
+            bgGradient="linear(to-r, teal.200, cyan.700)"
+            borderRadius="md"
+            style={{
+              padding: '1vh 0',
+              borderRadius: 'var(--chakra-radii-md) var(--chakra-radii-md) 0 0',
+            }}
+          >
+            <Heading size="lg" align="center" color="white">
+              Чат
+            </Heading>
+          </Box>
+          <Box flex="1" h="100%">
+            <Chat />
+          </Box>
+        </Flex>
       </Box>
     </Flex>
   );
