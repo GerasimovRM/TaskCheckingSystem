2016c6118f08673960651b2cb20e034a6b457909
Roman Gersaimov <romagrizly@gmail.com>
2021-04-25 02:00:03 +0500

    update auth service

--- backend/api/routers/auth.py
+++ backend/api/routers/auth.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, status, HTTPException
 import aiohttp
-from services import ResponseVkAccessToken
-from pydantic import ValidationError
-from datetime import timedelta
+import json
 
-from errors import BaseError
-from config import ACCESS_TOKEN_EXPIRE_MINUTES, VK_CLIENT_ID, VK_CLIENT_SECRET, VK_REDIRECT_URI
+from pydantic import ValidationError
+from fastapi import APIRouter, status, HTTPException
+
+from services import ResponseVkAccessToken
+from config import VK_CLIENT_ID, VK_CLIENT_SECRET, VK_REDIRECT_URI
 from database import User
-from services import create_token_user
+from services import create_token_user, get_vk_user_with_photo
 from models import UserDto
 
 
@@ -17,7 +17,7 @@ router = APIRouter(
 )
 
 
-@router.get("/login")
+@router.get("/login", response_model=UserDto)
 async def login(vk_code: str):
     async with aiohttp.ClientSession() as session:
         data = {
@@ -28,18 +28,25 @@ async def login(vk_code: str):
             "scope": "offline"
         }
         async with session.get("https://oauth.vk.com/access_token", params=data) as response:
-            data = await response.json()
+            response_data = await response.json()
             try:
-                # TODO: expires_in and access_token fields not used
-                response_vk_access_token = ResponseVkAccessToken(**data)
-                user = await User.objects.get_or_none(vk_id=response_vk_access_token.user_id)
-                if user:
-                    user.access_token = response_vk_access_token.access_token
-                    user.jwt_token = create_token_user(user)
-                    print(user.dict())
-                    return UserDto(**user.dict())
-                return response_vk_access_token
-            except ValidationError:
+                response_vk_access_token = ResponseVkAccessToken(**response_data)
+            except ValidationError as e:
                 raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail=BaseError(**data).dict())
+                    detail="bad access token")
+    try:
+        db_user = await User.objects.get_or_none(vk_id=response_vk_access_token.user_id)
+        if db_user:
+            db_user.access_token = response_vk_access_token.access_token
+            db_user.jwt_token = create_token_user(db_user)
+        else:
+            vk_user = await get_vk_user_with_photo(response_vk_access_token.access_token)
+            db_user = User(**vk_user.dict(), access_token=response_vk_access_token.access_token)
+            db_user.jwt_token = create_token_user(db_user)
+            await db_user.save()
+        return UserDto(**db_user.dict())
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=json.loads(e.json()))
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserDto
 from .token import Token, TokenData
+from .vk_user_with_photo import VkUserWithPhoto
