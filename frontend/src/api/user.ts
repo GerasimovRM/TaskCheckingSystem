36880fc5069760911a83d8b8c188604ba7383abc
mihett05 <mihett05@yandex.ru>
2021-06-11 09:54:47 +0500

    Updated eslint and added api

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,5 +1,1 @@
 import { request } from './api';
-
-const getUserData = () => {
-  return request('get', '/user/get_user_data/');
-};
--- frontend/src/store/userStore.ts
+++
@@ -1,7 +0,0 @@
-import { action, observable } from 'mobx';
-
-import User from '../models/User';
-
-class UserStore {
-  @observable public user: User | null = null;
-}
