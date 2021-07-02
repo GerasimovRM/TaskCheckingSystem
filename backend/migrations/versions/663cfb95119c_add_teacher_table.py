df31ab773da6ea3fff98e160f27495d3bcf41dd1
GerasimovRM <romagrizly@gmail.com>
2021-07-02 12:24:31 +0500

    fix alembic add teacher table

--- backend/migrations/versions/663cfb95119c_add_teacher_table.py
+++ backend/migrations/versions/663cfb95119c_add_teacher_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '663cfb95119c'
-down_revision = 'a26f13a43ee8'
+down_revision = '0b4a507ed621'
 branch_labels = None
 depends_on = None
 
