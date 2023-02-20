c66be8f1451e73a312a7419eab5e8864a4bfd7a1
Roman Gersaimov <romagrizly@gmail.com>
2023-02-21 00:56:56 +0500

    update config ports

--- backend/celery_worker/app.py
+++ backend/celery_worker/app.py
@@ -2,21 +2,21 @@ import os
 
 from celery import Celery
 
-from config import RABBITMQ_USER, RABBITMQ_PASSWORD, REDIS_PASSWORD
+from config import RABBITMQ_USER, RABBITMQ_PASSWORD, REDIS_PASSWORD, RABBITMQ_PORT, REDIS_PORT
 
 celery_app = None
 
 if not os.getenv("DOCKER"):
     celery_app = Celery(
         "worker",
-        broker=f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@localhost:5672//",
-        backend=f"redis://:{REDIS_PASSWORD}@localhost:6379/0"
+        broker=f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@localhost:{RABBITMQ_PORT}//",
+        backend=f"redis://:{REDIS_PASSWORD}@localhost:{REDIS_PORT}/0"
     )
 else:
     celery_app = Celery(
         "worker",
-        broker=f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@rabbitmq:5672//",
-        backend=f"redis://:{REDIS_PASSWORD}@redis:6379/0"
+        broker=f"amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@rabbitmq:{RABBITMQ_PORT}//",
+        backend=f"redis://:{REDIS_PASSWORD}@redis:{REDIS_PORT}/0"
     )
 celery_app.conf.task_routes = {
     "celery_worker.worker.test_celery": "check-solution-queue",
