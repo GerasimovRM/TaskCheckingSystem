e36ac1c96122edb477c24518bc838be2cb84c4c9
Roman Gersaimov <romagrizly@gmail.com>
2023-06-25 10:00:27 +0500

    remove celery from project

--- backend/celery_worker/languages/language_base.py
+++
@@ -1,9 +0,0 @@
-class LanguageBase:
-    def __init__(self, *args, **kwargs):
-        pass  # TODO: base items for check solution code from other languages
-
-    def run(self):
-        pass
-
-    def check_syntax(self):
-        pass
