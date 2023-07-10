a9b9c2d0580a584f8c0e1d427a22cb2aee586d9c
Roman M. Gerasimov <romagrizly@gmail.com>
2023-07-10 17:46:09 +0500

    hotfix prod: backend in url websocket solution

--- frontend/src/pages/TaskPage.tsx
+++ frontend/src/pages/TaskPage.tsx
@@ -125,7 +125,7 @@ const TaskPage = observer(() => {
         }
     }, [])
     useEffect(()=> {
-        ws.current = new WebSocket(`ws://localhost:5500/solution/${taskId}/${user?.id}/${courseId}/${groupId}`)
+        ws.current = new WebSocket(`ws://backend:8000/solution/${taskId}/${user?.id}/${courseId}/${groupId}`)
         //console.log(ws.current)
         const wsCurrent = ws.current
         return () => {
